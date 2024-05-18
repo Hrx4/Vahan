435a49ec767957dcf8ad5c507255f86880e6bcdb
Kamal <dragkamal71@gmail.com>
2024-05-18 14:09:17 +0530

    Readme changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import MainPage from "./pages/MainPage";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [alertOpen, setAlertOpen] = useState(true)
   useEffect(() => {
-    if(alertOpen){
-      console.log('====================================');
-      console.log('kdmf');
-      console.log('====================================');
-      alert('As i am using render for backend api , so it will take some time to make the api call first time....Please wait after calling the api.....')
-      return setAlertOpen(false)
-    }
-      
-  }, [])
-  
+    alert(
+      "As i am using render for backend api , so it will take some time to make the api call first time....Please wait after calling the api....."
+    );
+  }, []);
+
   return (
     <>
       <Navbar />
